48239c0d7b7fae1109997f7de979ab1ea0a9c40d
rabh1011 <saurabhblr10@gmail.com>
2026-01-07 12:41:38 +0530

    replacing class file with java

---
+++ Arrays/IndexOfLargestElement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IndexOfLargestElement {
+    public static void main(String[] args) {
+        int[] array = { 10, 5, 20, 8 };
+        int retValue = 0;
+
+        if ((retValue = indexOfLargestElement(array)) != -1) {
+            // FIX: Print 'retValue' (the index) not 'array[retValue]' (the value)
+            System.out.println("Index of largest element is: " + retValue);
+        }
+    }
+
+    static int indexOfLargestElement(int[] array) {
+        int index = 0;
+        if (array.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[index] < array[i]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
